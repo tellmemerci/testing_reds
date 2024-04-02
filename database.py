f97020b1c861720881116a2f701288a874eeff8b
Tim <tima.uramov@yandex.ru>
2024-04-02 22:41:13 +0300

    done full

--- database.py
+++ database.py
@@ -2,14 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI, HTTPException
-# Создание движка базы данных
+
 DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(DATABASE_URL)
-
-# Создание базового класса для объявления моделей
 Base = declarative_base()
-
-# Определение модели User
 class User(Base):
     __tablename__ = 'users'
 
@@ -19,16 +15,7 @@ class User(Base):
     nationality = Column(String)
     age = Column(Integer)
     email = Column(String)
-
-# Создание таблицы в базе данных
 Base.metadata.create_all(bind=engine)
 
-# Создание сессии с привязкой к движку
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db = SessionLocal()
-
-# Создание и добавление объекта в базу данных
-#Person5 = User(name="Урамов Тимофей Леонидович", sex="M", nationality="Russian", age=22, email="<EMAIL>")
-#db.add(Person5)
-#db.commit()
-#print("Количество записей в таблице users:", db.query(User).count())
